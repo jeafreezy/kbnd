bad3582bf3a6c69992b06184f9ff083f5239058c
jeafreezy <jolaiyaemmanuel@gmail.com>
2021-04-01 18:37:47 +0100

    Latest

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 const express = require('express');
-const app = express()
-const PORT = 3000;
-const { Client } = require('pg')
+const app = express();
+var cors = require('cors');
+const { Client } = require('pg');
+const Shopify = require('shopify-api-node');
 
-app.use(express.json())
+
+//Shopify client connection
+
+const shopify = new Shopify({
+    shopName: 'kushion-studio',
+    apiKey: process.env.API_KEY,
+    password: process.env.PASSWORD,
+    autoLimit: true,
+});
+
+// const PORT = 3000;
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(cors());
+app.use(express.json());
 
 const client = new Client({
 
-    //Heroku username
-    user: 'postgres',
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
+    
+    // user:'postgres',
 
-    //Enter heroku url
-    host: 'localhost',
+    // database:'postgres',
 
-    //Database name
-    database: 'postgres',
+    // host:'localhost',
+    
+    // port:5432,
 
-    //YOUR PASSWORD HERE
-    password: '',
+    // password:'2398'
 
-    port: 5432,
 
 });
 
@@ -33,18 +50,17 @@ const query = `CREATE TABLE IF NOT EXISTS users (
     date varchar,
     time varchar,
     variant varchar,
-    pin varchar,
+    token varchar,
+    productID varchar,
     map_details json
     );
     `;
 
 //Connecting to DB
-
 client.connect()
 
 
 //Executing Create Table Query
-
 client.query(query, (err, res) => {
     if (err) {
         console.error(err);
@@ -53,11 +69,20 @@ client.query(query, (err, res) => {
     console.log('Database Successfully started');
 })
 
+app.get('/', (req,res)=>{
+
+    res.json('It is working fine')
+}
+
+);
+
 
 //Route to create New user
-app.post('/', (req,res)=>{
+app.post('/register', (req,res)=>{
+
     let username = req.body.username.toLowerCase();
     let password = req.body.password;
+   
     let query = `INSERT INTO USERS VALUES(
             '${username}',
             '${password}')`
@@ -69,13 +94,15 @@ app.post('/', (req,res)=>{
         }else {
             console.log('User added');
             //Add page after register here
-            res.send("DONE")
+            res.send({success:'DONE'})
         }
     })
 })
 
+
 //Route for user login
 app.post('/login', (req,res,next)=>{
+   
     let username = req.body.username.toLowerCase();
     let password = req.body.password;
     let query =
@@ -90,17 +117,19 @@ app.post('/login', (req,res,next)=>{
             if(result.rows[0].password===password)
             {
                 //Show Login Success
-                res.send("USER LOGIN SUCCESS")
+                res.send({success:"USER LOGIN SUCCESS"})
                 return next();
             }else{
                 //Show login failed
-                res.json("USER LOGIN FAILED")
+                res.json({error:"USER LOGIN FAILED"})
                 return next();
             }
         }
     })
 })
-//To create a new Object
+
+
+//To create a new Map Object
 app.post('/mapInfo', (req,res)=>{
 
     let style = req.body.style;
@@ -108,7 +137,8 @@ app.post('/mapInfo', (req,res)=>{
     let center = req.body.center;
     let bearing = req.body.bearing;
     let pitch = req.body.pitch;
-    let pin = req.body.pin;
+    let token = req.body.token;
+    let productID = req.body.productID;
     let date = req.body.date;
     let time = req.body.time;
     let variantColor = req.body.variantColor;
@@ -116,26 +146,27 @@ app.post('/mapInfo', (req,res)=>{
 
     let variant = variantSize + "," + variantColor;
 
+    let centerLatLng=[center.lat,center.lng]
+    // let centerLatLng=[1,1]
+
     let map_details = JSON.stringify({
 
         "style":`${style}`,
         "zoom":`${zoom}`,
-        "center":`${center}`,
+        "center":`${centerLatLng}`,
         "bearing":`${bearing}`,
         "pitch":`${pitch}`
 
     });
 
 
-    let hasPin = pin !== "undefined" ? pin : 'Nil';
-
-
-    let query = `INSERT INTO mapinfo(date,time,variant,pin,map_details) VALUES(
+    let query = `INSERT INTO mapinfo(date,time,variant,token,productID,map_details) VALUES(
 
             '${date}',
             '${time}',
             '${variant}',
-            '${hasPin}',
+            '${token}',
+            '${productID}',
             '${map_details}');`;
 
     client.query(query,(err,result)=>{
@@ -147,34 +178,63 @@ app.post('/mapInfo', (req,res)=>{
             res.json({message:"Sorry there is some error"});
 
         }else {
+
             console.log('Object inserted');
             //Send response for inserted Object
-            res.json("Object Inserted")
+            res.json({success:"Object Inserted"})
         }
     })
 });
 
 
 
-//Display all objects
-app.get('/mapdata', (req,res,next)=>{
+//Fetch all items added stored in the mapinfo table
+
+app.get('/addedToCart', (req,res,next)=>{
+
     let query =
         `SELECT * FROM mapinfo;`
+
     client.query(query,(err,result)=>{
+
         if (err) {
+
             console.error(err);
+
             res.json({message:"Sorry there is some error"});
+
         }else {
+
             if (result.rows.length > 0) {
-                res.json({message: "Success", objects: result.rows})
-                return next();
+
+
+                res.json({message: "Success", payload: result.rows})
+
+                return next()
+                
+
             } else {
-                res.send("NO DATA AVAILABLE");
+
+                res.send({error:"NO DATA AVAILABLE"});
 
             }
         }
     })
 });
+
+
+//fetch the orders from shopify
+
+app.get('/orders',(req,res,next)=>{
+
+    shopify.order
+    .list()
+    .then(data=>res.json({message: "Success",payload:data}))
+    .catch((err) =>{console.error(err)});
+
+})
+
+
 
 //Delete an object
 app.delete('/delete/:id',(req,res)=>{
@@ -202,4 +262,6 @@ app.delete('/delete/:id',(req,res)=>{
 
 app.listen(process.env.PORT|| PORT,()=>{
     console.log(`Server running on port ${process.env.PORT}`)
+    // console.log(`Server running on port ${PORT}`)
+ 
 });
